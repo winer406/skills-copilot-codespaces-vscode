87352d5014833937a94028720a442bea5d554655
winer406 <107400957+winer406@users.noreply.github.com>
2023-07-20 05:13:17 +0000

    Copilot second commit

---
+++ member.js
@@ -0,0 +1,7 @@
+function skillsMember(){
+    var member = document.getElementById("member");
+    member.innerHTML = "Member";
+    member.style.color = "red";
+    member.style.fontWeight = "bold";
+    member.style.fontSize = "20px";
+}
